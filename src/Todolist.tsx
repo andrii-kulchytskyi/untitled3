eedd94604ac0d933dacb59084ef0c38a7ba32812
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2022-12-27 14:09:39 +0400

    1

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -18,15 +18,18 @@ export type TaskType = {
 
 export const Todolist = (props: PropsTodoList) => {
 
-    let [newTaskTitle, setNewTaskTitle] = useState("")
+    let [title, setTitle] = useState("")
 
     const addTaskHandler = () => {
-        props.addTask(newTaskTitle);
-        setNewTaskTitle("")
+        if (title.trim() === "") {
+            return;
+        }
+        props.addTask(title);
+        setTitle("")
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewTaskTitle(e.currentTarget.value)
+        setTitle(e.currentTarget.value)
     }
 
     const onClickAllFiltersHandler = () => props.changeFilter("all")
@@ -38,7 +41,7 @@ export const Todolist = (props: PropsTodoList) => {
         <div>
             <h3>{props.title}</h3>
             <input onChange={onChangeHandler}
-                   value={newTaskTitle}
+                   value={title}
             />
             <button onClick={addTaskHandler}>+
             </button>
